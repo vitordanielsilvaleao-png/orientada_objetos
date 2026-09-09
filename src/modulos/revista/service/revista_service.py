a72f1e34872773479447b1a2fc29dd75b36980c4
Gustavo Souza <gustavosouzags2865@gmail.com>
2026-09-09 19:40:18 -0300

    feat: implementa service e testes de integracao de revista

--- src/modulos/revista/service/revista_service.py
+++ src/modulos/revista/service/revista_service.py
@@ -1,27 +1,124 @@
-#Import das bibliotecas e classes necessárias para o funcionamento do sistema
+# Import das bibliotecas e classes necessárias para o funcionamento do sistema
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from compartilhado.base_service import BaseService
-from revista.schemas.schamas_revista import SchemaRevistaCadastro, SchemaRevistaAtualizacao
+
+from src.compartilhado.base_service import BaseService
+from src.modulos.revista.schemas.schamas_revista import (
+    SchemaRevistaCadastro,
+    SchemaRevistaAtualizacao
+)
+from src.modulos.material.entidades.editora import Editora
+from src.modulos.material.entidades.categoria import Categoria
+from src.modulos.revista.revista import Revista
 
 
-#Declaração da classe RevistaService
+# Declaração da classe RevistaService
 class RevistaService(BaseService):
 
-    #Declaração do construtor da classe
-    def __init__(self, session:Session):
+    # Declaração do construtor da classe
+    def __init__(self, session: Session):
         super().__init__(session)
 
-    def cadastrar(self, data:SchemaRevistaCadastro):
-        pass
+    # Método para cadastrar revistas
+    def cadastrar(self, data: SchemaRevistaCadastro):
 
+        editora_cadastrar = self.session.query(Editora).filter_by(
+            id=data.editora_id
+        ).first()
+
+        categoria_cadastrar = self.session.query(Categoria).filter_by(
+            id=data.categoria_id
+        ).first()
+
+        if not editora_cadastrar:
+            raise HTTPException(
+                status_code=404,
+                detail="A editora informada não existe"
+            )
+
+        if not categoria_cadastrar:
+            raise HTTPException(
+                status_code=404,
+                detail="A categoria informada não existe"
+            )
+
+        revista_cadastrar = Revista(
+            titulo=data.titulo,
+            ano_publi=data.ano_publi,
+            editora_id=data.editora_id,
+            categoria_id=data.categoria_id,
+            issn=data.issn,
+            edicao=data.edicao
+        )
+
+        self.salvar(revista_cadastrar)
+        self.session.refresh(revista_cadastrar)
+
+        return revista_cadastrar
+
+    # Método para visualizar revistas
     def visualizar(self):
-        pass
 
-    def atualizar(self, revista_id:int ,data:SchemaRevistaAtualizacao):
-        pass
+        return self.session.query(Revista).all()
 
-    def inativar(self, revista_id:int):
-        pass
+    # Método para atualizar revistas
+    def atualizar(
+        self,
+        revista_id: int,
+        data: SchemaRevistaAtualizacao
+    ):
 
-    def ativar(self, revista_id:int):
-        pass
+        revista_atualizar = self.session.query(Revista).filter_by(
+            id=revista_id
+        ).first()
+
+        if not revista_atualizar:
+            raise HTTPException(
+                status_code=404,
+                detail="Revista não encontrada"
+            )
+
+        revista_atualizar.atualizar(
+            titulo=data.titulo,
+            ano_publi=data.ano_publi,
+            categoria_id=data.categoria_id,
+            editora_id=data.editora_id,
+            edicao=data.edicao
+        )
+
+        self.session.commit()
+        self.session.refresh(revista_atualizar)
+
+        return revista_atualizar
+
+    # Método para inativar revistas
+    def inativar(self, revista_id: int):
+
+        revista_inativar = self.session.query(Revista).filter_by(
+            id=revista_id
+        ).first()
+
+        if not revista_inativar:
+            raise HTTPException(
+                status_code=404,
+                detail="Revista não encontrada"
+            )
+
+        revista_inativar.inativar()
+        self.session.commit()
+
+    # Método para ativar revistas
+    def ativar(self, revista_id: int):
+
+        revista_ativar = self.session.query(Revista).filter_by(
+            id=revista_id
+        ).first()
+
+        if not revista_ativar:
+            raise HTTPException(
+                status_code=404,
+                detail="Revista não encontrada"
+            )
+
+        revista_ativar.ativar()
+        self.session.commit()
