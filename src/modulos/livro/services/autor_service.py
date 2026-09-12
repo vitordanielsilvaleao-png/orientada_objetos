912712f7b39c282bd173b29cbeb89d968633f71d
henoli077 <hen.oli.077@gmail.com>
2026-09-12 15:20:53 -0300

    implementa métodos de AutorService
    Implementados os métodos da classe AutorService e a realização de testes de seus respectivos métodos:
    * Cadastro de autor
    * Visualização dos autores
    * Atualização do nome do autor

--- src/modulos/livro/services/autor_service.py
+++ src/modulos/livro/services/autor_service.py
@@ -1,19 +1,55 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-
+from src.modulos.livro.entidades.autor import Autor
 from compartilhado.base_service import BaseService
-
 
 class AutorService(BaseService):
 
-    # Declaração do construtor da classe LivroService
+    # Declaração do construtor da classe AutorService
     def __init__(self, session: Session):
         super().__init__(session)
 
     def cadastrar(self, data):
-        pass
+        autor_existente = self.session.query(Autor).filter_by(nome=data.nome).first()
+        if autor_existente:
+            raise HTTPException(
+                status_code=400,
+                detail="Já existe um autor cadastrado com este nome."
+            )
 
+        novo_autor = Autor(
+            nome=data.nome
+        )
+
+        self.salvar(novo_autor)
+        self.session.refresh(novo_autor)
+        
+        return novo_autor
+    
+    #Método para visualizar autores
     def visualizar(self):
-        pass
+        return self.session.query(Autor).all()
 
+    #Método para atualizar autor
     def atualizar(self, autor_id, nome_autor):
-        pass
+        autor_atualizar = self.session.query(Autor).filter_by(id=autor_id).first()
+    
+        if not autor_atualizar:
+            raise HTTPException(
+                status_code=404,
+                detail="Autor não encontrado."
+            )
+            
+        nome_existente = self.session.query(Autor).filter_by(nome=nome_autor).first()
+        if nome_existente and nome_existente.id != autor_id:
+            raise HTTPException(
+                status_code=400,            
+                detail="Já existe outro autor cadastrado com este nome."            
+            )
+        
+        autor_atualizar.nome = nome_autor
+                    
+        self.session.commit()
+        self.session.refresh(autor_atualizar)
+            
+        return autor_atualizar
